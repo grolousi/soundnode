c0dfe552096ad6b9829bbc49a5497c46d47b4895
Alexandre Dos Santos <alex@hackages.io>
2020-04-11 05:33:55 +0200

    removed build from git

--- backend/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log('Hello world!');
-var express = require("express");
-var app = express();
-app.listen(process.env.PORT || 8080, function () {
-    console.log('App listening on port 8080!');
-});
-//# sourceMappingURL=index.js.map
